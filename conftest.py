976ce5cc23548603a9374aea9b5d106c65300d32
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-04-06 17:34:46 -0700

    Check in updated tests

--- conftest.py
+++ conftest.py
@@ -3,9 +3,15 @@ from pytest_mysql import factories
 from sqlalchemy import create_engine
 from string import Template
 
+import mongomock
+
 from lume_services.data.model.db.mysql import MySQLConfig, MySQLService
 from lume_services.data.model.model_db_service import ModelDBService
+from lume_services.data.results.db.service import DBServiceConfig
+from lume_services.data.results.db.mongodb.service import MongodbService
+from lume_services.data.results.results_db_service import ResultsDBService
 
+from lume_services.data.results.db.mongodb.models import ModelDocs
 
 mysql_server = factories.mysql_proc()
 
@@ -72,7 +78,6 @@ def mysql_service(mysql_config):
     return mysql_service
 
 
-
 @pytest.mark.usefixtures("mysql_proc")
 @pytest.fixture(scope="module", autouse=True)
 def model_db_service(mysql_service, mysql_database, base_db_uri, mysql_proc):
@@ -95,11 +100,68 @@ def model_db_service(mysql_service, mysql_database, base_db_uri, mysql_proc):
         connection.execute(f"DROP DATABASE {mysql_database};")
 
 
+class MongomockResultsDBConfig(DBServiceConfig):
+    host: str= 'mongomock://localhost'
+    db: str= "localhost"
+    port: int = 27017
 
-"""
 
 @pytest.fixture(scope="session", autouse=True)
-def mongodb_results_db():
-    ...
+def mongodb_config():
+    return MongomockResultsDBConfig()
 
-"""
+@mongomock.patch(servers=(('localhost', 27017),))
+@pytest.fixture(scope="module", autouse=True)
+def mongodb_service(mongodb_config):
+    return MongodbService(mongodb_config)
+
+
+@pytest.fixture(scope="module", autouse=True)
+def results_db_service(mongodb_service):
+
+    results_db_service = ResultsDBService(mongodb_service, ModelDocs)
+
+    # no teardown needed because mock is module scoped
+    return results_db_service
+import pytest
+from datetime import datetime
+
+@pytest.fixture(scope="session", autouse=True)
+def test_generic_result_document():
+    return  {
+        "flow_id": "test_flow_id",
+        "inputs": {
+            "input1": 2.0,
+            "input2": [1,2,3,4,5],
+            "input3": "my_file.txt"
+        },
+        "outputs": {
+            "output1": 2.0,
+            "output2": [1,2,3,4,5],
+            "ouptut3": "my_file.txt"
+        },
+    }
+
+
+@pytest.fixture(scope="module", autouse=True)
+def test_impact_result_document():
+    return  {
+        "flow_id": "test_flow_id",
+        "inputs": {
+            "input1": 2.0,
+            "input2": [1,2,3,4,5],
+            "input3": "my_file.txt"
+        },
+        "outputs": {
+            "output1": 2.0,
+            "output2": [1,2,3,4,5],
+            "ouptut3": "my_file.txt"
+        },
+        "plot_file": "my_plot_file.txt",
+        "archive": "archive_file.txt",
+        "pv_collection_isotime": datetime.now(),
+        "config": {
+            "config1": 1,
+            "config2": 2
+        }
+    }
--- lume_services/tests/data/results/test_result_documents.py
+++ lume_services/tests/data/results/test_result_documents.py
@@ -1,60 +1,31 @@
-from lume_services.data.results.db.models.impact import ImpactResultDocument
-from lume_services.data.results.db.document import ResultDocument
+from lume_services.data.results.db.mongodb.models.impact import ImpactResultDocument
+from lume_services.data.results.db.mongodb.document import GenericResultDocument
 from datetime import datetime
-
-from mongoengine import ValidationError
 import pytest
 
 
-@pytest.fixture(autouse=True)
-def test_result_document():
-    return  {
-        "flow_id": "test_flow_id",
-        "inputs": {
-            "input1": 2.0,
-            "input2": [1,2,3,4,5],
-            "input3": "my_file.txt"
-        },
-        "outputs": {
-            "output1": 2.0,
-            "output2": [1,2,3,4,5],
-            "ouptut3": "my_file.txt"
-        },
-    }
+@pytest.mark.run(order=1)
+def test_construct_result_document(test_generic_result_document):
+    result_doc_instance = GenericResultDocument(**test_generic_result_document)
 
 
-@pytest.fixture(autouse=True)
-def test_impact_document():
-    return  {
-        "flow_id": "test_flow_id",
-        "inputs": {
-            "input1": 2.0,
-            "input2": [1,2,3,4,5],
-            "input3": "my_file.txt"
-        },
-        "outputs": {
-            "output1": 2.0,
-            "output2": [1,2,3,4,5],
-            "ouptut3": "my_file.txt"
-        },
-        "plot_file": "my_plot_file.txt",
-        "archive": "archive_file.txt",
-        "pv_collection_isotime": datetime.now(),
-        "config": {
-            "config1": 1,
-            "config2": 2
-        }
-    }
-
-
-def test_construct_result_document(test_result_document):
-    result_doc_instance = ResultDocument(**test_result_document)
-
-
-def test_construct_impact_result_document(test_impact_document, test_result_document):
-    result_doc_instance = ImpactResultDocument(**test_impact_document)
+@pytest.mark.run(order=1)
+def test_construct_impact_result_document(test_impact_result_document, test_generic_result_document):
 
     # check missing fields
-    with pytest.raises(ValidationError):
-        missing_field_doc = ImpactResultDocument(**test_result_document)
+    missing_field_doc = ImpactResultDocument(**test_generic_result_document)
+    validation_error = ImpactResultDocument.get_validation_error()
+    with pytest.raises(validation_error):
         missing_field_doc.validate()
+
+  #  del missing_field_doc
+    result_doc_instance = ImpactResultDocument(**test_impact_result_document)
+  #  del result_doc_instance
+
+def test_distinct_collection(test_impact_result_document, test_generic_result_document):
+
+    # confirm using distinct names for collection
+    result_doc_instance = GenericResultDocument(**test_generic_result_document)
+    impact_doc_instance = ImpactResultDocument(**test_impact_result_document)
+    
+    assert result_doc_instance._get_collection_name() != impact_doc_instance._get_collection_name()
