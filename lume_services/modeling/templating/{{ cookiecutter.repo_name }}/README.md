23b2e9e09b158aac85867777c7220715a55a4d39
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-04-12 20:17:20 -0700

    Check in base of template repository

---
+++ lume_services/modeling/templating/{{ cookiecutter.repo_name }}/README.md
@@ -0,0 +1,21 @@
+# {{ cookiecutter.repository_name }}
+
+## Installation notes
+
+Release artifacts...
+
+
+
+
+
+
+
+
+### Note
+This README was automatically generated using the template defined in https://github.com/slaclab/lume-services/lume_services/modeling/templating with the following configuration:
+
+```json
+{
+
+}
+```
---
+++ lume_services/modeling/templating/{{ cookiecutter.repo_name }}/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from os import path
+import versioneer
+
+cur_dir = path.abspath(path.dirname(__file__))
+
+# parse requirements
+with open(path.join(cur_dir, "requirements.txt"), "r") as f:
+    requirements = f.read().split()
+
+# set up additional dev requirements
+dev_requirements = []
+with open(path.join(cur_dir, "dev-requirements.txt"), "r") as f:
+    dev_requirements = f.read().split()
+
+# set up additional dev requirements
+docs_requirements = []
+with open(path.join(cur_dir, "docs-requirements.txt"), "r") as f:
+    docs_requirements = f.read().split()
+
+
+setup(
+    name="{{ cookiecutter.package_name }}",
+    author="{{ cookiecutter.author }}",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=find_packages(),
+    author_email="{{ cookiecutter.author }}",
+  #  license="...",
+    install_requires=requirements,
+    extras_require={"dev": dev_requirements, "docs": docs_requirements},
+    url="{{ cookiecutter.github_url }}",
+    include_package_data=True,
+    python_requires=">=3.7",
+    entry_points = {
+        "orchestration": [
+            "{{ cookiecutter.project_slug }}.model={{ cookiecutter.project_slug }}.model:{{ cookiecutter.model_name }}",
+            "{{ cookiecutter.project_slug }}.flow={{ cookiecutter.project_slug }}.flow.flow:{{ cookiecutter.model_name }}"
+        ]
+    }
+)
