39fff99ab9f09bfab1aa2d4d7edf21fc6ae04d5e
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-05-03 12:58:44 -0700

    Check in cli

--- lume_services/cli/cli.py
+++ lume_services/cli/cli.py
@@ -1,8 +1,10 @@
 import click
 from .modeling.schema import generate_schema_docs
 
+
 @click.group()
 def main():
     pass
+
 
 main.add_command(generate_schema_docs)
--- lume_services/cli/modeling/schema.py
+++ lume_services/cli/modeling/schema.py
@@ -1,9 +1,10 @@
 from lume_services.data.model.db.schema import generate_schema_graph, generate_uml_graph
 import click
 
+
 @click.command()
 @click.option("--uml_output_filename", required=True, type=str)
-@click.option('--schema_output_filename', required=True, type=str)
+@click.option("--schema_output_filename", required=True, type=str)
 def generate_schema_docs(schema_output_filename, uml_output_filename):
 
     generate_schema_graph(schema_output_filename)
