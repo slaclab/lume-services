a8f7454ded1d56244f12f0672b3e2083b094aa08
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-05-02 23:02:36 -0700

    Check in tests for file service

---
+++ lume_services/tests/data/file/test_local_filesystem.py
@@ -0,0 +1,53 @@
+import pytest
+
+def test_local_filesystem_dir_exists(local_filesystem_handler, tmp_path):
+
+    assert local_filesystem_handler.dir_exists(tmp_path, create_dir=False)
+
+
+def test_local_filesystem_create_dir(local_filesystem_handler, tmp_path):
+
+    new_tmp_dir = f"{tmp_path}/placeholder_dr"
+    local_filesystem_handler.create_dir(new_tmp_dir)
+
+    assert local_filesystem_handler.dir_exists(new_tmp_dir, create_dir=False)
+
+
+def test_local_filesystem_dir_exist_create(local_filesystem_handler, tmp_path):
+
+    new_tmp_dir = f"{tmp_path}/placeholder_dr"
+
+    # create dir
+    assert local_filesystem_handler.dir_exists(new_tmp_dir, create_dir=True)
+    assert local_filesystem_handler.dir_exists(new_tmp_dir, create_dir=False)
+
+
+
+def test_local_filesystem_read_write(local_filesystem_handler, tmp_path, text_serializer):
+
+    tmp_file = f"{tmp_path}/test.txt"
+    text = "test text"
+
+    #write
+    local_filesystem_handler.write(tmp_file, text, text_serializer)
+
+    assert local_filesystem_handler.file_exists(tmp_file)
+
+    # read
+    new_text = local_filesystem_handler.read(tmp_file, text_serializer)
+
+    assert new_text == text
+
+
+def test_local_filesystem_create_dir(local_filesystem_handler, tmp_path, text_serializer):
+
+
+    tmp_file = f"{tmp_path}/tmp_dir2/test.txt"
+    text = "test text"
+
+    # fail on no directory creation 
+    with pytest.raises(FileNotFoundError):
+        local_filesystem_handler.write(tmp_file, text, text_serializer, create_dir=False)
+
+    # succeed on creation
+    local_filesystem_handler.write(tmp_file, text, text_serializer, create_dir=True)
---
+++ lume_services/tests/scheduling/test_db_results.py
@@ -0,0 +1,42 @@
+import cloudpickle
+import pytest
+
+import prefect
+from prefect.engine.result import Result, NoResultType
+from prefect.engine.result.base import ResultNotImplementedError
+from lume_services.scheduling.prefect.results.db import DBResult
+
+
+def test_result_service_injection(test_generic_result_document, context):
+    result = DBResult(value= test_generic_result_document, model_type="Generic")
+
+    #check location is None
+    assert result.location is None
+
+
+def test_generic_result(results_service, test_generic_result_document):
+    result = DBResult(value= test_generic_result_document, model_type="Generic", results_service=results_service)
+
+    #check location is None
+    assert result.location is None
+
+
+def test_generic_everything_is_pickleable_after_init(results_service, test_generic_result_document):
+    result = DBResult(value= test_generic_result_document, model_type="Generic", results_service=results_service)
+
+    assert cloudpickle.loads(cloudpickle.dumps(result)) == result
+
+
+def test_impact_everything_is_pickleable_after_init(results_service, test_impact_result_document):
+    result = DBResult(value= test_impact_result_document, model_type="Impact", results_service=results_service)
+
+    assert cloudpickle.loads(cloudpickle.dumps(result)) == result
+
+
+def test_write_generic_result(results_service, test_generic_result_document):
+    result = DBResult(model_type="Generic", results_service=results_service)
+    result.write(test_generic_result_document)
+
+def test_write_impact_result(results_service, test_impact_result_document):
+    result = DBResult(model_type="Impact", results_service=results_service)
+    result.write(test_impact_result_document)
