eef9547ffdf8b4b5277bfecdb8c65e27b97b2e74
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-05-02 23:05:09 -0700

    Check in config base class for settings

---
+++ lume_services/config.py
@@ -0,0 +1,11 @@
+from pydantic import BaseSettings
+
+
+
+class LUMESettings(BaseSettings):
+    """Base class for enforcing common prefix on all LUMESettings
+    
+    """
+
+    class Config:
+        env_prefix = 'LUME_'
