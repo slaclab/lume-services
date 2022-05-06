a8e0bb37493f6525023f9ca4123ced6bb0896566
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-05-06 15:27:41 -0700

    Include template files

--- lume_services/modeling/templating/template/{{ cookiecutter.repo_name }}/setup.py
+++ lume_services/modeling/templating/template/{{ cookiecutter.repo_name }}/setup.py
@@ -13,28 +13,24 @@ dev_requirements = []
 with open(path.join(cur_dir, "dev-requirements.txt"), "r") as f:
     dev_requirements = f.read().split()
 
-# set up additional dev requirements
-docs_requirements = []
-with open(path.join(cur_dir, "docs-requirements.txt"), "r") as f:
-    docs_requirements = f.read().split()
-
-
 setup(
     name="{{ cookiecutter.project_slug }}",
     author="{{ cookiecutter.author }}",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     packages=find_packages(),
-  #  license="...",
+    #  license="...",
     install_requires=requirements,
-    extras_require={"dev": dev_requirements, "docs": docs_requirements},
+    extras_require={"dev": dev_requirements},
     url="{{ cookiecutter.github_url }}",
     include_package_data=True,
     python_requires=">=3.7",
-    entry_points = {
+    entry_points={
         "orchestration": [
-            "{{ cookiecutter.project_slug }}.model={{ cookiecutter.project_slug }}.model:{{ cookiecutter.model_class }}",
-            "{{ cookiecutter.project_slug }}.flow={{ cookiecutter.project_slug }}.flow.flow:get_flow"
+            "{{ cookiecutter.project_slug }}.model=\
+                {{ cookiecutter.project_slug }}.model:{{ cookiecutter.model_class }}",
+            "{{ cookiecutter.project_slug }}.flow=\
+                {{ cookiecutter.project_slug }}.flow.flow:get_flow",
         ]
-    }
+    },
 )
