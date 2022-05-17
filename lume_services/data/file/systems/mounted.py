c23bb15191adccdad1da609f84220845bd18a05a
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-05-17 14:11:39 -0700

    Format mounted fs

--- lume_services/data/file/systems/mounted.py
+++ lume_services/data/file/systems/mounted.py
@@ -1,4 +1,3 @@
-import os
 from typing import Any
 from enum import Enum
 import logging
@@ -37,14 +36,17 @@ class PathNotInMount(Exception):
         self.filesystem_identifier = filesystem_identifier
         self.mount_path = mount_path
         self.mount_alias = mount_alias
-        self.message = f"Path {path} not in mount for mounted filesystem identifier: {filesystem_identifier}, Mount path: {mount_path}, Mount alias: {mount_alias}"
+        self.message = f"Path {path} not in mount for mounted filesystem identifier:\
+            {filesystem_identifier}, Mount path: {mount_path}, Mount alias: \
+                {mount_alias}"
         super().__init__(self.message)
 
 
 class MountedFilesystem(LocalFilesystem):
-    """Handler for mounted filesystem. Modifies the LocalFilesystem to implements checks for mount path modifications.
-    Container and container orchestration tools often allow the ability to provide an alias for a mounted directory.
-    This handler accounts for the mount base and verifies that the file is within the path.
+    """Handler for mounted filesystem. Modifies the LocalFilesystem to implements
+    checks for mount path modifications. Container and container orchestration tools
+    often allow the ability to provide an alias for a mounted directory. This handler
+    accounts for the mount base and verifies that the file is within the path.
 
     """
 
@@ -52,9 +54,9 @@ class MountedFilesystem(LocalFilesystem):
         """Initialize mounted filesystem with optional custom identifier.
 
         Args:
-            mount_path (str):
-            mount_alias (str):
-            identifier (str):
+            mount_path (str): Mount path on mounted filesystem
+            mount_alias (str): Directory alias used internally for mount
+            identifier (str): String identifier for filesystem
 
         """
         if not identifier:
@@ -113,7 +115,8 @@ class MountedFilesystem(LocalFilesystem):
 
         Args:
             filepath (str): Path of file to read
-            serializer (SerializerBase): Implementation of lume-base SerializerBase abstract base class
+            serializer (SerializerBase): Implementation of lume-base SerializerBase
+                abstract base class
 
         """
         filepath = self._check_mounted_path(filepath)
@@ -131,7 +134,8 @@ class MountedFilesystem(LocalFilesystem):
         Args:
             filepath (str):
             object (Any): Object to serialize
-            serializer (SerializerBase): Implementation of lume-base SerializerBase abstract base class
+            serializer (SerializerBase): Implementation of lume-base SerializerBase
+                abstract base class
             create_dir (bool): Whether to create directory in case not implemented
 
         """
@@ -139,7 +143,8 @@ class MountedFilesystem(LocalFilesystem):
         super().write(filepath, object, serializer, create_dir=create_dir)
 
     def _check_mounted_path(self, path: str):
-        """Checks that the path exists inside the mount point relative to mount path or alias.
+        """Checks that the path exists inside the mount point relative to mount path or
+            alias.
 
         Args:
             path (str): Path to validate
@@ -147,8 +152,6 @@ class MountedFilesystem(LocalFilesystem):
         """
 
         if self._mount_path in path:
-            print(path)
-            print(path.replace(self._mount_path, self._mount_alias))
             return path.replace(self._mount_path, self._mount_alias)
 
         elif self._mount_alias in path:
