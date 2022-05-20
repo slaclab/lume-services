51551618d79460a112cb8e0b04be3aa11814c292
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-05-19 19:55:53 -0700

    Check in updated config

--- lume_services/scheduling/schema/flow_of_flows_schema.py
+++ lume_services/scheduling/schema/flow_of_flows_schema.py
@@ -1,9 +1,13 @@
 from pydantic import BaseModel, validator
 from prefect.backend.flow import FlowView
 from typing import List, Optional, Dict
-from prefect.tasks.prefect import create_flow_run, wait_for_flow_run, get_task_run_result
+from prefect.tasks.prefect import (
+    create_flow_run,
+    wait_for_flow_run,
+    get_task_run_result,
+)
 from prefect import Flow as PrefectFlow
-from prefect import Parameter, Task
+from prefect import Parameter
 
 # Pydantic schema describing flow of flows composition
 
@@ -69,23 +73,30 @@ class FlowOfFlows(BaseModel):
                 flow["name"], project_name=flow["project_name"], last_updated=True
             ).flow
 
-            # organize data 
-            parameters = {parameter.name: parameter for parameter in deserialized_flow.parameters()}
+            # organize data
+            parameters = {
+                parameter.name: parameter
+                for parameter in deserialized_flow.parameters()
+            }
 
             mapped_parameters = None
             if flow.get("mapped_parameters") is not None:
-                mapped_parameters = {param["name"]: param for param in flow.get("mapped_parameters")}
+                mapped_parameters = {
+                    param["name"]: param for param in flow.get("mapped_parameters")
+                }
 
-            task_slugs = {task.name: task.slug for task in deserialized_flow.get_tasks()}
+            task_slugs = {
+                task.name: task.slug for task in deserialized_flow.get_tasks()
+            }
 
             # compose flow objects
             flows[flow["name"]] = Flow(
-                name = flow["name"],
-                prefect_flow = deserialized_flow,
-                parameters = parameters,
-                task_slugs = task_slugs,
-                project_name = flow["project_name"],
-                mapped_parameters=mapped_parameters
+                name=flow["name"],
+                prefect_flow=deserialized_flow,
+                parameters=parameters,
+                task_slugs=task_slugs,
+                project_name=flow["project_name"],
+                mapped_parameters=mapped_parameters,
             )
 
         # validate flow parameters
@@ -116,7 +127,13 @@ class FlowOfFlows(BaseModel):
 
         return flows
 
-    def compose(self):
+    def compose(self) -> PrefectFlow:
+        """Compose Prefect flow from FlowOfFlows object.
+
+        Returns:
+            PrefectFlow
+
+        """
 
         # compose flow of flows
         with PrefectFlow(self.name) as flow_of_flows:
@@ -131,10 +148,15 @@ class FlowOfFlows(BaseModel):
                 flow_params = {}
                 for param_name, param in flow.parameters.items():
                     # do not register mapped parameters as flow parameters
-                    if flow.mapped_parameters is not None and param_name in flow.mapped_parameters:
+                    if (
+                        flow.mapped_parameters is not None
+                        and param_name in flow.mapped_parameters
+                    ):
                         continue
 
+                    # update name and slug
                     param.name = f"{flow_name}-{param_name}"
+                    param.slug = f"{flow_name}-{param_name}"
                     params[param.name] = param
 
                     # use original param name for flow config
@@ -142,9 +164,13 @@ class FlowOfFlows(BaseModel):
 
                 # set up entry task
                 if i == 0:
-                    flow_run = create_flow_run(flow_name=flow_name, project_name=flow.project_name, parameters=flow_params)
+                    flow_run = create_flow_run(
+                        flow_name=flow_name,
+                        project_name=flow.project_name,
+                        parameters=flow_params,
+                    )
 
-                #task_run_result = flow_run.get_task_run_result(slug=...)
+                # setup other tasks
                 elif i > 0:
 
                     # create references to parameters
@@ -153,10 +179,14 @@ class FlowOfFlows(BaseModel):
 
                         # update flow_params with mapping
                         for param_name, mapped_param in flow.mapped_parameters.items():
-                            task_slug = self.composing_flows[mapped_param.parent_flow_name].task_slugs[mapped_param.parent_task_name]
+                            task_slug = self.composing_flows[
+                                mapped_param.parent_flow_name
+                            ].task_slugs[mapped_param.parent_task_name]
 
                             # use task run result as input param
-                            task_run_result = get_task_run_result(flow_runs[mapped_param.parent_flow_name], task_slug)
+                            task_run_result = get_task_run_result(
+                                flow_runs[mapped_param.parent_flow_name], task_slug
+                            )
 
                             # track param name with result
                             flow_params[param_name] = task_run_result
@@ -164,38 +194,32 @@ class FlowOfFlows(BaseModel):
                             # add flow to upstream
                             upstream_flows.add(mapped_param.parent_flow_name)
 
-                        print("Creating flow")
-                        print(flow_params)
-                        print(flow_name)
-                        flow_run = create_flow_run(flow_name=flow_name, project_name=flow.project_name, parameters=flow_params)
-                        print("Created")
+                        flow_run = create_flow_run(
+                            flow_name=flow_name,
+                            project_name=flow.project_name,
+                            parameters=flow_params,
+                        )
 
-                    # configure upstreams
+                    # configure upstreams if any
                     for upstream in upstream_flows:
                         flow_run.set_upstream(flow_waits[upstream])
-            
+
                 flow_wait = wait_for_flow_run(flow_run, raise_final_state=True)
                 flow_runs[flow_name] = flow_run
                 flow_waits[flow_name] = flow_wait
 
-            print("Composed flow")
-
-                # 
-
-
         # validate flow of flows
         flow_of_flows.validate()
-        flow_id = flow_of_flows.register(project_name=self.project_name)
+
+        # flow_id = flow_of_flows.register(project_name=self.project_name)
         return flow_of_flows
 
     def compose_local(self):
         """
-        
+
         Note:
-            Prefect 1.0 does not allow subflow run without previous registration with 
+            Prefect 1.0 does not allow subflow run without previous registration with
             the server. This function is a workaround, but will be massively simplified
             once moved to Prefect 2.0, which does support direct subflow run.
         """
         ...
-
-
