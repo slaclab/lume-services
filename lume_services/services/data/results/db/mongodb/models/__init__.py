bdc479204c1de75882e908c54d8f89940016a4a2
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-06-17 11:53:49 -0700

    Remove all leftover documents

--- lume_services/services/data/results/db/mongodb/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from lume_services.data.results.db.mongodb.models.impact import ImpactResultDocument
-from lume_services.data.results.db.mongodb.document import GenericResultDocument
-
-
-ModelDocs = {"Generic": GenericResultDocument, "Impact": ImpactResultDocument}
