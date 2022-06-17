73703326626e55eabe2e72e538332ade960728bc
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-06-17 11:42:08 -0700

    Check in updated files

--- lume_services/tests/files/__init__.py
+++ lume_services/tests/files/__init__.py
@@ -3,3 +3,11 @@ from pkg_resources import resource_filename
 PYTEST_DOCKER_COMPOSE = resource_filename(
     "lume_services.tests.files", "docker-compose.yml"
 )
+
+SAMPLE_IMAGE_FILE = resource_filename("lume_services.tests.files", "sample_image.png")
+
+SAMPLE_YAML_FILE = resource_filename("lume_services.tests.files", "sample_yaml.yml")
+
+SAMPLE_IMPACT_ARCHIVE = resource_filename(
+    "lume_services.tests.files", "sample_impact_archive.h5"
+)
