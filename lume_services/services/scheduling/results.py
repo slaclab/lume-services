abeb452574735f865b7185549e6330cb72bffbd2
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-06-23 13:49:35 -0700

    Test all task results

--- lume_services/services/scheduling/results.py
+++ lume_services/services/scheduling/results.py
@@ -1,58 +1,99 @@
 import logging
 from dependency_injector.wiring import Provide
 from prefect import task
-import json
+from typing import Any
 
 from lume_services.context import Context
 from lume_services.services.data.results import ResultsDB
 from lume_services.services.data.files import FileService
+from prefect.engine.results import PrefectResult
 
-from lume_services.data.files import get_file_from_serializer_string
-from lume_services.data.results import get_result_from_string
+from lume_services.data.files import get_file_from_serializer_string, File
+from lume_services.data.results import get_result_from_string, Result
 
 logger = logging.getLogger(__name__)
 
 
-@task()
-def load_db_result_task(
-    result_json_rep, results_db_service: ResultsDB = Provide[Context.results_db_service]
-):
-
-    json_loaded = json.loads(result_json_rep)
-    result_type = get_result_from_string(json_loaded["result_type_string"])
-
-    result = result_type.parse_raw(result_json_rep)
-
-    return result.load_result(results_db_service=results_db_service)
-
-
-@task()  # SAVE W/ PREFECT RESULT
+@task(log_stdout=True, result=PrefectResult())  # SAVE W/ PREFECT RESULT
 def save_db_result_task(
-    result_json_rep, results_db_service: ResultsDB = Provide[Context.results_db_service]
-):
-    json_loaded = json.loads(result_json_rep)
-    result_type = get_result_from_string(json_loaded["result_type_string"])
+    result: Result, results_db_service: ResultsDB = Provide[Context.results_db_service]
+) -> dict:
+    """Insert result into the results database service. Creates a PrefectResult that
+    contains minimal representative information for reconstruction.
 
-    result = result_type.parse_raw(result_json_rep)
+    Args:
+        result (Result): Result object to save
+        results_db_service (ResultsDB): Results database service
 
-    return result.load_result(results_db_service=results_db_service)
+
+    Returns:
+        dict: Unique representation for collecting results.
+
+    """
+
+    result.insert(results_db_service=results_db_service)
+    return result.unique_rep()
+
+
+@task(log_stdout=True)
+def load_db_result_task(
+    result_rep, results_db_service: ResultsDB = Provide[Context.results_db_service]
+) -> Result:
+    """
+
+    Args:
+        result_rep (dict): Result representation containing result_type_string and query
+        results_db_service (ResultsDB): Results database service
+
+    Returns:
+        Result
+
+    Note: requires instructions for accessing attributtes
+
+
+    """
+    result_type = get_result_from_string(result_rep["result_type_string"])
+    result = result_type.load_from_query(
+        result_rep["query"], results_db_service=results_db_service
+    )
+
+    return result
 
 
 @task()
 def load_file_task(
-    file_json_rep, file_service: FileService = Provide[Context.file_service]
-):
+    file_rep, file_service: FileService = Provide[Context.file_service]
+) -> Any:
+    """Load a file
 
-    json_loaded = json.loads(file_json_rep)
-    result_type = get_file_from_serializer_string(json_loaded["serializer_type"])
+    Args:
+        file_rep (dict): File data representation
+        file_service (FileService): File service for interacting w/ filesystems
 
-    file_result = result_type.parse_raw(file_json_rep)
+    Returns:
+        Any: Unserialize file object
+
+    """
+
+    file_type = get_file_from_serializer_string(file_rep["file_type_string"])
+    file_result = file_type(**file_rep)
 
     return file_result.read(file_service=file_service)
 
 
-@task()  # SAVE W/ PREFECT RESULT
+@task(log_stdout=True, result=PrefectResult())
 def save_file_task(
-    file, results_db_service: ResultsDB = Provide[Context.results_db_service]
+    file: File, file_service: FileService = Provide[Context.file_service]
 ):
-    ...
+    """Save a file
+
+    Args:
+        file (File): File object
+        file_service (FileService): File service for interacting w/ filesystems
+
+    Returns:
+        dict: Loaded file type
+
+    """
+    file.write(file_service=file_service)
+    return file.jsonable_dict()
---
+++ lume_services/tests/scheduling/test_result_tasks.py
@@ -0,0 +1,113 @@
+from lume_services.services.scheduling.results import (
+    load_db_result_task,
+    save_db_result_task,
+    load_file_task,
+    save_file_task,
+)
+from lume_services.tests.files import SAMPLE_TEXT_FILE
+from lume_services.data.files import TextFile
+from prefect import Flow, task
+
+
+class TestDBTaskResults:
+    def test_save_db_result_task(self, results_db_service, generic_result):
+
+        with Flow("save_db_result_task_flow") as flow:
+            my_task = save_db_result_task(
+                generic_result, results_db_service=results_db_service
+            )
+
+        flow_run = flow.run()
+
+        assert flow_run.is_successful()
+        assert flow_run.result[my_task].is_successful()
+        assert flow_run.result[my_task].result == generic_result.unique_rep()
+
+    def test_load_db_result_task(self, results_db_service, generic_result):
+
+        result_rep = generic_result.unique_rep()
+
+        with Flow("load_db_result_task_flow") as flow:
+            my_task = load_db_result_task(
+                result_rep, results_db_service=results_db_service
+            )
+
+        flow_run = flow.run()
+
+        assert flow_run.is_successful()
+        assert flow_run.result[my_task].is_successful()
+        assert flow_run.result[my_task].result == generic_result
+
+    def test_db_result_propogation(self, results_db_service, generic_result):
+
+        result_rep = generic_result.unique_rep()
+
+        @task
+        def return_result(result):
+            return result
+
+        with Flow("load_db_result_task_flow") as flow:
+            my_task = load_db_result_task(
+                result_rep, results_db_service=results_db_service
+            )
+            downstream_task = return_result(my_task)
+
+        flow_run = flow.run()
+
+        assert flow_run.is_successful()
+        assert flow_run.result[my_task].is_successful()
+        assert flow_run.result[downstream_task].is_successful()
+        assert flow_run.result[downstream_task].result == generic_result
+
+
+class TestFileTaskResults:
+    def test_save_file_task(self, tmp_path, file_service):
+
+        filepath = f"{tmp_path}/tmp_file.txt"
+        text_file = TextFile(
+            obj="text", filename=filepath, file_system_identifier="local"
+        )
+
+        with Flow("save_file_task_flow") as flow:
+            my_task = save_file_task(text_file, file_service=file_service)
+
+        flow_run = flow.run()
+        assert flow_run.is_successful()
+        assert flow_run.result[my_task].is_successful()
+        assert flow_run.result[my_task].result == text_file.jsonable_dict()
+
+    def test_load_file_task(self, file_service):
+
+        text_file = TextFile(filename=SAMPLE_TEXT_FILE, file_system_identifier="local")
+        text_file_rep = text_file.jsonable_dict()
+
+        with Flow("load_file_task_flow") as flow:
+            my_task = load_file_task(text_file_rep, file_service=file_service)
+
+        flow_run = flow.run()
+        assert flow_run.is_successful()
+        assert flow_run.result[my_task].is_successful()
+        assert flow_run.result[my_task].result == text_file.read(
+            file_service=file_service
+        )
+
+    def test_file_result_propogation(self, file_service):
+
+        text_file = TextFile(filename=SAMPLE_TEXT_FILE, file_system_identifier="local")
+        text_file_rep = text_file.jsonable_dict()
+
+        # @task
+        def return_text(text):
+            return text
+
+        with Flow("load_file_task_flow") as flow:
+            my_task = load_file_task(text_file_rep, file_service=file_service)
+            downstream_task = return_text(my_task)
+
+        flow_run = flow.run()
+        assert flow_run.is_successful()
+        assert flow_run.result[my_task].is_successful()
+        assert flow_run.result[downstream_task].is_successful()
+        assert flow_run.result[downstream_task].result == text_file.read(
+            file_service=file_service
+        )
