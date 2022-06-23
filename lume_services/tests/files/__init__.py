585a93e75e2e15e12bb8d071c5b86bd8ab813d6a
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-06-23 12:27:11 -0700

    Check in text test file

--- lume_services/tests/files/__init__.py
+++ lume_services/tests/files/__init__.py
@@ -4,6 +4,8 @@ PYTEST_DOCKER_COMPOSE = resource_filename(
     "lume_services.tests.files", "docker-compose.yml"
 )
 
+SAMPLE_TEXT_FILE = resource_filename("lume_services.tests.files", "sample_text.txt")
+
 SAMPLE_IMAGE_FILE = resource_filename("lume_services.tests.files", "sample_image.png")
 
 SAMPLE_YAML_FILE = resource_filename("lume_services.tests.files", "sample_yaml.yml")
