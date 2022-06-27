5fe6ade3ede421e3eb38faac1d86d5d61de11d30
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-06-27 13:52:16 -0700

    Check in docker backend

--- lume_services/services/scheduling/backends/__init__.py
+++ lume_services/services/scheduling/backends/__init__.py
@@ -1,2 +1,4 @@
 from .backend import Backend
 from .kubernetes import KubernetesBackend
+from .local import LocalBackend
+from .docker import DockerBackend
---
+++ lume_services/services/scheduling/backends/docker.py
@@ -0,0 +1,59 @@
+from pydantic import BaseModel
+from prefect.run_configs import DockerRun
+from typing import Optional, Union
+import logging
+
+from lume_services.services.scheduling.backends import Backend
+
+logger = logging.getLogger(__name__)
+
+
+class DockerResourceRequest(BaseModel):
+    # CPU shares (relative weight).
+    cpu_shares: int = None
+    # CPUs in which to allow execution (0-3, 0,1).
+    cpuset_cpus: int = None
+    # Microseconds of CPU time that the container can get in a CPU period.
+    cpu_quota: int = None
+    # The length of a CPU period in microseconds.
+    cpu_period: int = None
+    # Memory limit. Accepts float values (which represent the memory limit of the
+    # created container in bytes) or a string with a units identification char
+    # (100000b, 1000k, 128m, 1g).
+    mem_limit: Union[float, str] = None
+    # Memory soft limit.
+    mem_reservation: Union[float, str] = None
+    # Tune a container’s memory swappiness behavior. Accepts number between 0 and 100.
+    mem_swappiness: int = None
+    #  Maximum amount of memory + swap a container is allowed to consume.
+    memswap_limit: Union[str, int]
+
+
+class DockerHostConfig:
+    resource_request: Optional[DockerResourceRequest]
+    privileged: bool = False
+    read_only: bool = False
+
+
+# can extend to use
+# https://docker-py.readthedocs.io/en/stable/api.html#docker.api.container.ContainerApiMixin.create_host_config
+class DockerRunConfig:
+    image: str
+    env: Optional[dict]
+    # labels:
+    host_config: DockerHostConfig
+
+    # can extend to use
+    # https://docker-py.readthedocs.io/en/stable/api.html#docker.api.container.ContainerApiMixin.create_host_config
+
+
+class DockerBackend(Backend):
+    # default image
+    default_image: str = None
+
+    def get_run(
+        self,
+        run_config: DockerRunConfig,
+    ):
+
+        return DockerRun(**run_config.dict(exclude_none=True))
