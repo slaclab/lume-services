5eb3c809dbfe24da027bf38cf834b0da996b5b1e
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-06-27 12:57:15 -0700

    Check in docker compose

--- conftest.py
+++ conftest.py
@@ -8,10 +8,8 @@ def pytest_addoption(parser):
     parser.addini("mysql_user", default="root", help="MySQL user")
     parser.addini("mysql_password", default="root", help="MySQL password")
     parser.addini(name="mysql_dbname", help="Mysql database name", default="test")
-    parser.addini(name="mysql_params", help="MySQL params", default="")
     parser.addini("mysql_database", default="model_db", help="Model database name")
     parser.addini("mysql_poolsize", default=1, help="MySQL client poolsize")
-    parser.addini(name="mysql_mysqld", help="mysqld command", default="mysqld")
 
     parser.addini("mongodb_host", default="127.0.0.1", help="MySQL host")
     parser.addini("mongodb_port", default=3306, help="MySQL port")
@@ -19,33 +17,33 @@ def pytest_addoption(parser):
     parser.addini("mongodb_password", default="root", help="MySQL password")
     parser.addini(name="mongodb_dbname", help="Mysql database name", default="test")
 
+    # prefect
+    parser.addini(name="postgres_db", help="Prefect postgres db", default="prefect_db")
     parser.addini(
-        name="mysql_mysqld_safe", help="mysqld safe command", default="mysqld_safe"
-    )
-    parser.addini(name="mysql_admin", help="mysql admin command", default="mysqladmin")
-    parser.addini(
-        name="mysql_logsdir",
-        help="Add log directory",
+        name="postgres_user", help="Prefect postgres user", default="prefect_user"
     )
     parser.addini(
-        name="mysql_install_db",
-        help="Installation path",
-        default="mysql_install_db",
+        name="postgres_password",
+        help="Prefect postgres password",
+        default="prefect_password",
+    )
+    parser.addini(name="apollo_host_port", help="Prefect apollo api port", default=4200)
+    parser.addini(
+        name="hasura_host_port", help="Prefect hasura host port", default=3000
+    )
+    parser.addini(
+        name="postgres_host_port", help="Prefect postgres host port", default=3000
+    )
+    parser.addini(
+        name="graphql_host_port", help="Prefect graphql host port", default=4201
     )
 
 
 @pytest.fixture(scope="session", autouse=True)
-def mysql_user(request):
-    user = request.config.getini("mysql_user")
-    os.environ["PYTEST_MYSQL_USER"] = user
-    return user
-
-
-@pytest.fixture(scope="session", autouse=True)
-def mysql_password(request):
-    user = request.config.getini("mysql_password")
-    os.environ["PYTEST_MYSQL_PASSWORD"] = user
-    return user
+def rootdir(request):
+    rootdir = request.config.rootpath
+    os.environ["LUME_SERVICES_ROOTDIR"] = str(rootdir)
+    return rootdir
 
 
 from glob import glob
