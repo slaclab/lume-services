224faeaf27dcd360cd6cc6e32343ba4d8cb1afc0
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-07-05 14:23:51 -0700

    Move mounts to conftest

--- lume_services/tests/fixtures/services/files.py
+++ lume_services/tests/fixtures/services/files.py
@@ -1,25 +1,6 @@
 import pytest
 
-from lume_services.services.data.files.filesystems import (
-    LocalFilesystem,
-    MountedFilesystem,
-)
 from lume_services.services.data.files import FileService
-
-
-@pytest.fixture(scope="module")
-def mount_path(tmp_path_factory):
-    return str(tmp_path_factory.mktemp("mounted_dir"))
-
-
-@pytest.fixture(scope="module", autouse=True)
-def local_filesystem_handler():
-    return LocalFilesystem()
-
-
-@pytest.fixture(scope="module", autouse=True)
-def mounted_filesystem_handler(mount_path):
-    return MountedFilesystem(mount_path=mount_path, mount_alias="/User/my_user/data")
 
 
 @pytest.fixture(scope="module")
