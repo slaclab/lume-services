19da1bbd9898bfa268315e9bce2500f338fadad2
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-07-06 11:17:07 -0700

    Change task names

--- lume_services/tests/scheduling/test_tasks.py
+++ lume_services/tests/scheduling/test_tasks.py
@@ -1,22 +1,27 @@
 from prefect import Flow, task
+from dependency_injector.containers import DynamicContainer
 
 from lume_services.services.scheduling.tasks import (
-    load_db_result_task,
-    save_db_result_task,
-    load_file_task,
-    save_file_task,
+    load_db_result,
+    save_db_result,
+    load_file,
+    save_file,
+    configure_services,
 )
 from lume_services.tests.files import SAMPLE_TEXT_FILE
 from lume_services.data.files import TextFile
-from lume_services.tests.fixtures.services.scheduling import *  # noqa: F403, F401
+from lume_services.tests.fixtures.services.results import *  # noqa: F403, F401
 from lume_services.tests.fixtures.services.files import *  # noqa: F403, F401
+from lume_services.tests.fixtures.services.scheduling import *  # noqa: F403, F401
+
+from lume_services import config
 
 
 class TestDBTaskResults:
     def test_save_db_result_task(self, results_db_service, generic_result):
 
         with Flow("save_db_result_task_flow") as flow:
-            my_task = save_db_result_task(
+            my_task = save_db_result(
                 generic_result, results_db_service=results_db_service
             )
 
@@ -31,9 +36,7 @@ class TestDBTaskResults:
         result_rep = generic_result.unique_rep()
 
         with Flow("load_db_result_task_flow") as flow:
-            my_task = load_db_result_task(
-                result_rep, results_db_service=results_db_service
-            )
+            my_task = load_db_result(result_rep, results_db_service=results_db_service)
 
         flow_run = flow.run()
 
@@ -50,9 +53,7 @@ class TestDBTaskResults:
             return result
 
         with Flow("load_db_result_task_flow") as flow:
-            my_task = load_db_result_task(
-                result_rep, results_db_service=results_db_service
-            )
+            my_task = load_db_result(result_rep, results_db_service=results_db_service)
             downstream_task = return_result(my_task)
 
         flow_run = flow.run()
@@ -71,7 +72,7 @@ class TestFileTaskResults:
         text_file = TextFile(obj=text, filename=filepath, filesystem_identifier="local")
 
         with Flow("save_file_task_flow") as flow:
-            my_task = save_file_task(
+            my_task = save_file(
                 obj=text,
                 file_type=TextFile,
                 filename=filepath,
@@ -90,7 +91,7 @@ class TestFileTaskResults:
         text_file_rep = text_file.jsonable_dict()
 
         with Flow("load_file_task_flow") as flow:
-            my_task = load_file_task(text_file_rep, file_service=file_service)
+            my_task = load_file(text_file_rep, file_service=file_service)
 
         flow_run = flow.run()
         assert flow_run.is_successful()
@@ -104,12 +105,12 @@ class TestFileTaskResults:
         text_file = TextFile(filename=SAMPLE_TEXT_FILE, filesystem_identifier="local")
         text_file_rep = text_file.jsonable_dict()
 
-        # @task
+        @task
         def return_text(text):
             return text
 
         with Flow("load_file_task_flow") as flow:
-            my_task = load_file_task(text_file_rep, file_service=file_service)
+            my_task = load_file(text_file_rep, file_service=file_service)
             downstream_task = return_text(my_task)
 
         flow_run = flow.run()
@@ -119,3 +120,22 @@ class TestFileTaskResults:
         assert flow_run.result[downstream_task].result == text_file.read(
             file_service=file_service
         )
+
+
+class TestServiceConfiguration:
+    @classmethod
+    def setup_class(cls):
+        config.context = None
+
+    def test_service_config_task(self):
+
+        assert config.context is None
+
+        with Flow("configure") as flow:
+            my_task = configure_services()
+
+        flow_run = flow.run()
+        assert flow_run.is_successful()
+        assert flow_run.result[my_task].is_successful()
+        assert config.context is not None
+        assert isinstance(config.context, (DynamicContainer,))
