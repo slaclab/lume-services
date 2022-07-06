cf6cfb5fab7701386bb68186c002fbd530e5db13
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-07-06 16:23:25 -0700

    Change file service scope

--- lume_services/tests/fixtures/services/files.py
+++ lume_services/tests/fixtures/services/files.py
@@ -3,7 +3,7 @@ import pytest
 from lume_services.services.data.files import FileService
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def file_service(local_filesystem_handler, mounted_filesystem_handler):
     filesystems = [local_filesystem_handler, mounted_filesystem_handler]
 
