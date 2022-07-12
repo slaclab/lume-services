8e1b302713d7bb72c9d6c8435ab4a5c676eaf06c
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-07-12 14:45:15 -0700

    Check in scheduling service

--- lume_services/services/scheduling/service.py
+++ lume_services/services/scheduling/service.py
@@ -1,17 +1,15 @@
-from prefect import Client, config as prefect_config
-from prefect.backend import FlowRunView
-from typing import List, Optional
+from datetime import timedelta
+from typing import Optional, Dict, Any
 
 from pydantic import BaseModel
+
+from prefect import Client, Flow, config as prefect_config
+from prefect.backend import FlowRunView
+from prefect.backend.flow_run import watch_flow_run
+
 from lume_services.services.data.files.service import FileService
 
-
 from lume_services.services.scheduling.backends.backend import Backend
-
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from lume_services.services.scheduling.flows import FlowOfFlows, Flow
 
 
 import logging
@@ -21,6 +19,7 @@ logger = logging.getLogger(__name__)
 # weekday_schedule = CronSchedule(
 #    "30 9 * * 1-5", start_date=pendulum.now(tz="US/Eastern")
 # )
+
 
 class PrefectGraphQLConfig(BaseModel):
     host: str = "0.0.0.0"
@@ -36,15 +35,21 @@ class PrefectServerConfig(BaseModel):
     host_port = "4200"
     host_ip = "127.0.0.1"
 
+
 class PrefectHasuraConfig(BaseModel):
     host: str = "localhost"
     port: str = "3000"
     host_port: str = "3000"
-    admin_secret: str = "" # a string. One will be automatically generated if not provided.
+    admin_secret: str = (
+        ""  # a string. One will be automatically generated if not provided.
+    )
     claims_namespace: str = "hasura-claims"
-    graphql_url: str  = "http://${server.hasura.host}:${server.hasura.port}/v1alpha1/graphql"
+    graphql_url: str = (
+        "http://${server.hasura.host}:${server.hasura.port}/v1alpha1/graphql"
+    )
     ws_url: str = "ws://${server.hasura.host}:${server.hasura.port}/v1alpha1/graphql"
     execute_retry_seconds: str = 10
+
 
 class PrefectUIConfig(BaseModel):
     host: str = "http://localhost"
@@ -53,6 +58,7 @@ class PrefectUIConfig(BaseModel):
     host_ip: str = "127.0.0.1"
     endpoint: str = "${server.ui.host}:${server.ui.port}"
     apollo_url: str = "http://localhost:4200/graphql"
+
 
 class PrefectTelemetryConfig(BaseModel):
     enabled: bool = True
@@ -70,12 +76,44 @@ class PrefectConfig(BaseModel):
     debug: bool = False
 
     def apply(self):
-        prefect_config.update(backend=self.backend.backend_type, home_dir=self.home_dir, debug=self.debug)
+        prefect_config.update(
+            backend=self.backend.backend_type, home_dir=self.home_dir, debug=self.debug
+        )
         prefect_config.server.update(**self.server.dict())
         prefect_config.server.graphql.update(**self.graphql.dict())
         prefect_config.server.hasura.update(**self.hasura.dict())
         prefect_config.server.ui.update(**self.ui.dict())
         prefect_config.server.telemetry.update(**self.telemetry.dict())
+
+
+class TaskNotCompletedError(Exception):
+    def __init__(self, task_slug: str, flow_id: str, flow_run_id: str):
+        self.flow_id = flow_id
+        self.flow_run_id = flow_run_id
+        self.task_slug = task_slug
+        self.message = (
+            "Task with slug: %s not completed for flow_run_id: %s, flow_id: %s."
+        )
+        super().__init__(self.message, self.task_slug, self.flow_run_id, self.flow_id)
+
+
+class EmptyResultError(Exception):
+    def __init__(self, flow_id: str, flow_run_id: str, task_slug: Optional[str]):
+        self.flow_id = flow_id
+        self.flow_run_id = flow_run_id
+        self.task_slug = task_slug
+        if not self.task_slug:
+            self.message = (
+                "Task with slug: %s for flow run: %s of flow_id: %s has no result."
+            )
+            super().__init__(
+                self.message, self.task_slug, self.flow_run_id, self.flow_id
+            )
+
+        else:
+            self.message = "Flow run: %s of flow_id: %s has no result."
+            super().__init__(self.message, self.flow_run_id, flow_id)
+
 
 class SchedulingService:
     """Scheduler handling job submission with Prefect."""
@@ -107,7 +145,7 @@ class SchedulingService:
 
     def register_flow(
         self,
-        flow: "Flow",
+        flow: Flow,
         project_name: str,
         image_tag: str,
     ) -> str:
@@ -127,17 +165,6 @@ class SchedulingService:
 
         return flow_id
 
-    def register_flow_of_flows(self, flow_of_flows: "FlowOfFlows") -> str:
-        """Register flow-of-flows in series.
-
-        Args:
-            flow_of_flows (FlowOfFlows): Instantiated flow of flows schema object
-
-        """
-
-        flow = flow_of_flows.compose_and_register()
-        return flow
-
     def schedule_run(
         self,
         flow_id: str,
@@ -156,7 +183,9 @@ class SchedulingService:
         # prefect.core.flow.Flow.run(parameters=None, run_on_schedule=None,
         #  runner_cls=None, **kwargs)
 
-        run_config = self.config.backend_config.get_run_config(file_service=self.file_service)
+        run_config = self.config.backend_config.get_run_config(
+            file_service=self.file_service
+        )
 
         flow_run_id = self._client.create_flow_run(
             flow_id=flow_id, parameters=data, run_config=run_config
@@ -164,41 +193,66 @@ class SchedulingService:
 
         return flow_run_id
 
-    def schedule_and_return_run(
-        self,        
+    def schedule_and_return(
+        self,
         flow_id: str,
-        data: dict = None,
+        task_slug: Optional[str],
+        data: Optional[Dict[str, Any]],
+        timeout: timedelta = timedelta(minutes=1),
+        cancel_on_timeout: bool = True,
     ):
         """
 
         Args:
-            flow_name (str):
+            flow_id (str):
             project_name (str):
+            task_slug (str): Slug of task to return
             data (dict): Dictionary mapping flow parameter name to value
+            timeout ()
+            cancel_on_timeout (bool=True): Whether to cancel execution on timeout error.
         """
 
-        schedule_run = self._client.create_flow_run(
+        run_config = self.config.backend_config.get_run_config(
+            file_service=self.file_service
+        )
+
+        flow_run_id = self._client.create_flow_run(
             flow_id=flow_id, parameters=data, run_config=run_config
         )
-        
-        ...
-        # BROKEN
 
-        # https://docs.prefect.io/api/latest/tasks/prefect.html?#wait-for-flow-run
+        flow_run = FlowRunView.from_flow_run_id(flow_run_id)
 
-        with Flow("schedule-run") as flow:
-            flow_run_id = self._client.create_flow_run(
-                flow_name=flow_name, project_name=project_name, parameters=data
-            )
+        try:
+            watch_flow_run(flow_run_id, stream_logs=True, max_duration=timeout)
+        except RuntimeError as err:
+            if cancel_on_timeout:
+                self._client.cancel_flow_run()
+            raise err
 
-            slug = flow.serialize()["tasks"][0]["slug"]
+        # get task run
+        if task_slug is not None:
+            task_run = flow_run.get_task_run(task_slug=task_slug)
+            if not task_run.state.is_successful():
+                raise TaskNotCompletedError(task_slug, flow_id, flow_run_id)
 
-            # slug should be absorbed into
-            # child_data = get_task_run_result(flow_run_id, slug)
-            # print(child_data)
+            res = task_run.get_result()
+            if res is None:
+                raise EmptyResultError(flow_id, flow_run_id, task_slug)
 
-            res = self._client.wait_for_flow_run(flow_run_id)
-            # child_data = get_task_run_result(flow_run_id, slug)
+        # assume flow result
+        else:
+            if not flow_run.state._result:
+                raise EmptyResultError(flow_id, flow_run_id)
 
-        flow_runs = FlowRunView._query_for_flow_run(where={"flow_id": {"_eq": id}})
+            return flow_run.state._result.value
 
+
+def _get_task_slugs_from_name(flow, name):
+
+    return flow.get_tasks(name)
+
+
+def _get_task_slugs_from_run_name(flow_run, name):
+    return [
+        task.task_slug for task in flow_run.get_all_task_runs() if task.name == name
+    ]
