487c132a7579a155282a38ea846591b370ac1eb6
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-07-13 13:06:09 -0700

    Add error file

---
+++ lume_services/errors.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+
+# Scheduling errors
+
+
+class TaskNotCompletedError(Exception):
+    def __init__(self, task_slug: str, flow_id: str, flow_run_id: str):
+        self.flow_id = flow_id
+        self.flow_run_id = flow_run_id
+        self.task_slug = task_slug
+        self.message = (
+            "Task with slug: %s not completed for flow_run_id: %s, flow_id: %s."
+        )
+        super().__init__(self.message, self.task_slug, self.flow_run_id, self.flow_id)
+
+
+class EmptyResultError(Exception):
+    def __init__(self, flow_id: str, flow_run_id: str, task_slug: Optional[str]):
+        self.flow_id = flow_id
+        self.flow_run_id = flow_run_id
+        self.task_slug = task_slug
+        if not self.task_slug:
+            self.message = (
+                "Task with slug: %s for flow run: %s of flow_id: %s has no result."
+            )
+            super().__init__(
+                self.message, self.task_slug, self.flow_run_id, self.flow_id
+            )
+
+        else:
+            self.message = "Flow run: %s of flow_id: %s has no result."
+            super().__init__(self.message, self.flow_run_id, flow_id)
