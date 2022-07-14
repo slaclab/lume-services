b71d7892bd45992d03aedec4ff1e5cb9e746d083
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-07-14 12:24:47 -0700

    Initialize job template

--- lume_services/services/scheduling/files/__init__.py
+++ lume_services/services/scheduling/files/__init__.py
@@ -1,12 +1,19 @@
 from pkg_resources import resource_filename
+import yaml
 
-# from lume_services.files import YAMLFile
-
-KUBERNETES_JOB_TEMPLATE_FILE = resource_filename(
+_KUBERNETES_JOB_TEMPLATE_FILE = resource_filename(
     "lume_services.services.scheduling.files", "kubernetes_job.yaml"
 )
 
-# KUBERNETES_JOB_TEMPLATE_FILE = YAMLFile(
-#    filename=_kubernetes_job_template_filename,
-#    filesystem_identifier="local"
-# )
+KUBERNETES_JOB_TEMPLATE = None
+
+
+def _initialize_job_template():
+    global KUBERNETES_JOB_TEMPLATE, _KUBERNETES_JOB_TEMPLATE_FILE
+    if KUBERNETES_JOB_TEMPLATE is None:
+        with open(_KUBERNETES_JOB_TEMPLATE_FILE, "r") as f:
+            KUBERNETES_JOB_TEMPLATE = yaml.safe_load(f)
+
+
+if not KUBERNETES_JOB_TEMPLATE:
+    _initialize_job_template()
