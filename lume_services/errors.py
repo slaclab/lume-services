2cfbfd77de68c8ee22e043903274deda59e22d7a
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-07-14 11:52:19 -0700

    Check in errors

--- lume_services/errors.py
+++ lume_services/errors.py
@@ -1,7 +1,31 @@
-from typing import Optional
+from typing import Optional, List
 
 
 # Scheduling errors
+
+
+class ParameterNotInFlowError(Exception):
+    def __init__(self, parameter_name: str, flow_name: str):
+        self.flow_name = flow_name
+        self.parameter_name = parameter_name
+        self.message = "Parameter %s not in flow %s."
+        super().__init__(self.message, self.parameter_name, self.flow_name)
+
+
+class ParentFlowNotInFlowsError(Exception):
+    def __init__(self, flow_name: str, flows: List[str]):
+        self.flow_name = flow_name
+        self.flows = flows
+        self.message = "Parent flow %s not in flows: %s"
+        super().__init__(self.message, self.flow_name, ", ".join(self.flows))
+
+
+class TaskNotInFlowError(Exception):
+    def __init__(self, flow_name: str, task_name: str):
+        self.flow_name = flow_name
+        self.task_name = task_name
+        self.message = "Task %s not in flow %s."
+        super().__init__(self.message, self.task_name, self.flow_name)
 
 
 class TaskNotCompletedError(Exception):
@@ -31,3 +55,9 @@ class EmptyResultError(Exception):
         else:
             self.message = "Flow run: %s of flow_id: %s has no result."
             super().__init__(self.message, self.flow_run_id, flow_id)
+
+
+class LocalBackendError(Exception):
+    def __init__(self):
+        self.message = "Cannot run server-backend operation using LocalBackend."
+        super().__init__(self.message)
