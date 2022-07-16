38c75de8970967f89922c24b98f0a93fe0817a7d
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-07-15 17:06:02 -0700

    Check in failure flow

--- lume_services/tests/files/flows/failure_flow.py
+++ lume_services/tests/files/flows/failure_flow.py
@@ -1,6 +1,5 @@
 from prefect import task, Flow
 from prefect.storage import Module
-from lume_services.files import TextFile
 import logging
 
 logger = logging.getLogger(__name__)
@@ -11,7 +10,6 @@ logger.setLevel(logging.DEBUG)
 @task
 def failure_task():
     assert 1 == 2
-
 
 
 with Flow("flow1", storage=Module(__name__)) as flow:
