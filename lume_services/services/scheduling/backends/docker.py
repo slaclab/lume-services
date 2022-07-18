400fa0fd65551886f43aefe83091a4dbbea18a92
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-07-18 12:56:36 -0700

    Validate w/ api version and use list instead of iterable

--- lume_services/services/scheduling/backends/docker.py
+++ lume_services/services/scheduling/backends/docker.py
@@ -1,5 +1,5 @@
 from pydantic import validator, Field
-from typing import Optional, Iterable, Dict, Any
+from typing import Optional, List, Dict, Any
 import logging
 
 from prefect.run_configs import DockerRun
@@ -7,6 +7,8 @@ from prefect.run_configs import DockerRun
 from docker.types import HostConfig
 from lume_services.services.scheduling.backends.backend import RunConfig
 from lume_services.services.scheduling.backends.server import ServerBackend
+
+from lume_services.utils import docker_api_version
 
 logger = logging.getLogger(__name__)
 
@@ -16,22 +18,22 @@ class DockerRunConfig(RunConfig):
     https://docs.prefect.io/api/latest/run_configs.html#dockerrun
 
     Attributes:
-        labels (Optional[Iterable[str]]): an iterable of labels to apply to this run
+        labels (Optional[List[str]]): an list of labels to apply to this run
             config. Labels are string identifiers used by Prefect Agents for selecting
             valid flow runs when polling for work
         env (Optional[dict]): Additional environment variables to set on the job
         image (str): Tag of image in which flow should run.
-        host_config (Dict[str, Any]): SignatureModel generated from docker HostConfig type
-            representing runtime args to be passed to Docker agent.
-            Full documentation of args can be found here: https://docker-py.readthedocs.io/en/stable/api.html#docker.api.container.ContainerApiMixin.create_host_config
-        ports (Optional[Iterable[int]]): An iterable of ports numbers to expose on
+        host_config (Optional[Dict[str, Any]]): Dictionary representing runtime args
+            to be passed to Docker agent. Full documentation of args can be found here:
+            https://docker-py.readthedocs.io/en/stable/api.html#docker.api.container.ContainerApiMixin.create_host_config
+        ports (Optional[List[int]]): An list of ports numbers to expose on
             container.
 
     """  # noqa
 
     image: str
-    host_config: Dict[str, Any]
-    ports: Optional[Iterable[int]]
+    host_config: Dict[str, Any] = None
+    ports: Optional[List[int]]
 
     @validator("host_config", pre=True)
     def validate_host_config(cls, v):
@@ -39,10 +41,13 @@ class DockerRunConfig(RunConfig):
         values.
 
         """
-
         if isinstance(v, (dict,)):
-            # test host config composition, will perform validation
-            HostConfig(**v)
+            # test host config composition using api version
+            try:
+                HostConfig(version=docker_api_version(), **v)
+            except Exception as e:
+                logger.exception(e)
+                raise e
 
         return v
 
