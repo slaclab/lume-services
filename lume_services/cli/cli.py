e9ec3b2d3fd68a4128d79b6053f84fd12b00b8ad
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-08-03 10:51:31 -0700

    Check in docker cli

--- lume_services/cli/cli.py
+++ lume_services/cli/cli.py
@@ -1,4 +1,5 @@
 import click
+from .docker import start_docker_services
 
 
 @click.group()
@@ -6,4 +7,24 @@ def main():
     pass
 
 
-# main.add_command()
+main.add_command(start_docker_services)
+
+"""
+import click
+
+CONTEXT_SETTINGS = dict(
+    default_map={'runserver': {'port': 5000}}
+)
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+def cli():
+    pass
+
+@cli.command()
+@click.option('--port', default=8000)
+def runserver(port):
+    click.echo(f"Serving on http://127.0.0.1:{port}/")
+
+if __name__ == '__main__':
+    cli()
+"""
---
+++ lume_services/cli/docker.py
@@ -0,0 +1,24 @@
+import click
+from lume_services.docker.compose import run_docker_services
+import time
+
+
+@click.group()
+def docker():
+    pass
+
+
+@docker.command()
+@click.option("--project_name", default="lume-services")
+def start_docker_services(project_name):
+    try:
+        with run_docker_services(project_name=project_name):
+            while True:
+                time.sleep(2)
+                print("Services started")
+    except KeyboardInterrupt:
+        print("Shutting down")
+
+
+if __name__ == "__main__":
+    docker()
