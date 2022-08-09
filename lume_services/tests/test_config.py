c5a7ed4bc57dab6eaad47466c6f9e97d0d2c2a19
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-08-09 13:56:08 -0700

    Move Prefect config test to config test

--- lume_services/tests/test_config.py
+++ lume_services/tests/test_config.py
@@ -1,16 +1,16 @@
 import pytest
 import os
 
-from lume_services.files import TextFile
+import prefect
+from prefect.utilities.backend import load_backend, save_backend
 
 from dependency_injector.containers import DynamicContainer
 
 from lume_services import config
 from lume_services.results import Result
+from lume_services.files import TextFile
 from lume_services.errors import EnvironmentNotConfiguredError
 from lume_services.services.files.filesystems.mounted import MountedFilesystem
-
-from prefect.utilities.backend import load_backend, save_backend
 
 
 class TestLumeSettings:
@@ -230,3 +230,57 @@ class TestResultServiceInjection:
         assert generic_result.flow_id == new_generic_result.flow_id
         assert generic_result.inputs == new_generic_result.inputs
         assert generic_result.outputs == new_generic_result.outputs
+
+
+class TestPrefectConfig:
+    def test_prefect_config(self, lume_services_settings):
+        prefect_config = lume_services_settings.prefect
+
+        # check that server has been applied
+        backend_spec = load_backend()
+        assert backend_spec["backend"] == "server"
+
+        # check assignment, has already been applied
+        assert prefect.config.debug == prefect_config.debug
+        assert prefect.config.home_dir == prefect_config.home_dir
+
+        # check server values
+        for key, value in prefect_config.server.dict().items():
+            attr = getattr(prefect.config.server, key)
+            assert attr == value
+
+        # check ui values
+        for key, value in prefect_config.ui.dict().items():
+            attr = getattr(prefect.config.server.ui, key)
+            assert attr == value
+
+        # check graphql values
+        for key, value in prefect_config.telemetry.dict().items():
+            attr = getattr(prefect.config.server.telemetry, key)
+            assert attr == value
+
+    def test_prefect_update_config(self, lume_services_settings):
+        config.configure(lume_services_settings)
+
+        prefect_config = lume_services_settings.prefect
+
+        new_config = prefect_config.copy()
+        new_config.server.host = "0.0.0.0"
+        new_config.server.host_port = 4000
+
+        new_config.apply()
+
+        # check server values
+        for key, value in prefect_config.server.dict().items():
+            attr = getattr(prefect.config.server, key)
+            assert attr == value
+
+        # check ui values
+        for key, value in prefect_config.ui.dict().items():
+            attr = getattr(prefect.config.server.ui, key)
+            assert attr == value
+
+        # check graphql values
+        for key, value in prefect_config.telemetry.dict().items():
+            attr = getattr(prefect.config.server.telemetry, key)
+            assert attr == value
