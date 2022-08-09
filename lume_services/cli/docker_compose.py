2231356afa02dfdee6cbce7fb66a8997c4a90e23
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-08-09 14:59:34 -0700

    Clean up docker-compose command

--- lume_services/cli/docker_compose.py
+++ lume_services/cli/docker_compose.py
@@ -1,4 +1,5 @@
 import click
+from lume_services.config import LUMEServicesSettings
 from lume_services.docker.compose import run_docker_services
 import time
 
@@ -10,14 +11,28 @@ def docker():
 
 @docker.command()
 @click.option("--project_name", default="lume-services")
-def start_docker_services(project_name):
+@click.option("--timeout", default=60.0)
+@click.option("--pause", default=1.0)
+def start_docker_services(project_name, timeout, pause):
+    """CLI utility for spinning up LUME-services with docker-compose. Services will
+    exit on KeyboardInterrupt (Ctrl+C).
+
+    Args:
+        project_name
+        timeout
+        pause
+
+    """
+    lume_services_settings = LUMEServicesSettings()
     try:
-        with run_docker_services(project_name=project_name, ui=True):
+        with run_docker_services(
+            lume_services_settings, timeout, pause, project_name=project_name, ui=True
+        ):
+            print("All services started and passing health checks.")
             while True:
                 time.sleep(2)
-                print("Services started")
     except KeyboardInterrupt:
-        print("Shutting down")
+        print("Shutting down LUME-services ")
 
 
 if __name__ == "__main__":
