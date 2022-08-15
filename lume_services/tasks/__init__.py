253bd62fee4fb0bb7b98167b168dac436feadd33
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-08-15 13:55:04 -0700

    Add check task to init

--- lume_services/tasks/__init__.py
+++ lume_services/tasks/__init__.py
@@ -1,3 +1,7 @@
 from .db import SaveDBResult, LoadDBResult
 from .file import LoadFile, SaveFile
-from .configure import configure_lume_services, prepare_lume_model_variables
+from .configure import (
+    configure_lume_services,
+    prepare_lume_model_variables,
+    check_local_execution,
+)
