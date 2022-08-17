4bf02b735d5248a3f662bbcc55758a8d17f923df
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-08-17 15:33:39 -0700

    Check in docker-compose updates

--- lume_services/docker/files/__init__.py
+++ lume_services/docker/files/__init__.py
@@ -1,3 +1,4 @@
 from pkg_resources import resource_filename
 
 DOCKER_COMPOSE = resource_filename("lume_services.docker.files", "docker-compose.yml")
+MODEL_DB_INIT = resource_filename("lume_services.docker.files", "model-db-init.sql")
---
+++ scripts/update_docker_compose_schema.py
@@ -0,0 +1,57 @@
+from lume_services.services.models.db.schema import (
+    Base,
+    Model,
+    Deployment,
+    Flow,
+    Project,
+    FlowOfFlows,
+)
+from lume_services.docker.files import MODEL_DB_INIT
+from sqlalchemy import create_mock_engine, create_engine
+from sqlalchemy.sql import compiler
+import click
+
+
+def build_db_schema() -> str:
+    """Utility function for creating db init for docker-compose."""
+
+    buffer = []
+
+    def executor(sql, *a, **kw):
+        stmt = sql.compile(dialect=engine.dialect)
+        buffer.append(stmt)
+
+    engine = create_mock_engine("mysql://", executor)
+
+    Base.metadata.create_all(engine)
+
+    return [str(buf) for buf in buffer]
+
+
+@click.command()
+@click.option("--filename", required=False, type=str, default=str(MODEL_DB_INIT))
+def build_docker_compose_schema(filename: str):
+
+    print(filename)
+
+    commands = build_db_schema()
+
+    with open(filename, "w") as f:
+        for command in commands:
+
+            command = command[:-2]  # drop newline at end
+            f.write(command + ";")
+
+        f.write("\n")
+
+
+@click.group()
+def main():
+    pass
+
+
+main.add_command(build_docker_compose_schema)
+
+
+if __name__ == "__main__":
+    main()
