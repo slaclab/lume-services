fa89e892d3e13a724b9d1e601427e7818822aa88
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-09-07 15:10:48 -0700

    Check in image

--- lume_services/services/scheduling/backends/server.py
+++ lume_services/services/scheduling/backends/server.py
@@ -57,6 +57,7 @@ class PrefectConfig(BaseModel):
     home_dir: str = "~/.prefect"
     debug: bool = False
     backend: Literal["server", "cloud"] = "server"
+    default_image: str = Field("jgarrahan/lume-services-prefect:latest", alias="image")
 
     def apply(self):
         prefect_config.update(
@@ -93,7 +94,6 @@ class ServerBackend(Backend):
     """
 
     config: PrefectConfig
-    default_image: str = Field(None, alias="image")
 
     class Config:
         underscore_attrs_are_private = True
