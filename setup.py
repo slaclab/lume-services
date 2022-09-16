deeedb4512d499e3fc2d3ac6cce5113f41eeb83d
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-09-16 12:04:30 -0700

    Remove data files

--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ with open(path.join(cur_dir, "requirements.txt"), "r") as f:
 
 setup(
     name="lume-services",
+    description="A set of orchestration tools for the LUME project",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     packages=find_packages(exclude="*tests*"),
-    author="SLAC National Accelerator Laboratory",
+    author="Jacqueline Garrahan",
     author_email="jgarra@slac.stanford.edu",
     license="SLAC Open",
     install_requires=requirements,
     url="https://github.com/slaclab/lume-services",
-    include_package_data=True,
     python_requires=">=3.7",
     entry_points={"console_scripts": ["lume-services = lume_services.cli.cli:main"]},
 )
