fe1df5ad92e431e855a937e9cdce1c95947a9488
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-09-23 14:11:20 -0700

    Add validator to file results

--- lume_services/results/impact.py
+++ lume_services/results/impact.py
@@ -2,9 +2,9 @@ from datetime import datetime
 
 from typing import Optional
 
-from pydantic import Field
+from pydantic import Field, validator
 from lume_services.results.generic import Result
-from lume_services.files import HDF5File, ImageFile
+from lume_services.files import HDF5File, ImageFile, get_file_from_serializer_string
 
 
 class ImpactResult(Result):
@@ -15,3 +15,13 @@ class ImpactResult(Result):
     archive: HDF5File
     pv_collection_isotime: datetime
     config: dict
+
+    @validator("plot_file", "archive", pre=True)
+    def validate_file(cls, v):
+
+        # if the plot file isinstance of dictionary, load file using file_type_string
+        if isinstance(v, dict):
+            file_type = get_file_from_serializer_string(v["file_type_string"])
+            return file_type(**v)
+
+        return v
