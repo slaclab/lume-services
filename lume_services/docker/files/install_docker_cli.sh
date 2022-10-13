40f58016bdf222c2cab23be47d3767ae9f1c45f8
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-10-13 16:06:17 -0700

    Correct SCR login and fix model bug

---
+++ lume_services/docker/files/install_docker_cli.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# From: https://tutorials.releaseworksacademy.com/learn/the-simple-way-to-run-docker-in-docker-for-ci
+apt-get update && \
+apt-get -y install apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common && \
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg > /tmp/dkey; apt-key add /tmp/dkey && \
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+   $(lsb_release -cs) \
+   stable" && \
+apt-get update && \
+apt-get -y install docker-ce
