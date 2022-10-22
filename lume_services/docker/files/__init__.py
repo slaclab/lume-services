484a6117e0ff16006d7beaf0d584b641e0af3fcc
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-10-22 02:31:39 -0700

    Remove unusedd ENV YAML

--- lume_services/docker/files/__init__.py
+++ lume_services/docker/files/__init__.py
@@ -2,4 +2,3 @@ from pkg_resources import resource_filename
 
 DOCKER_COMPOSE = resource_filename("lume_services.docker.files", "docker-compose.yml")
 MODEL_DB_INIT = resource_filename("lume_services.docker.files", "model-db-init.sql")
-ENVIRONMENT_YAML = resource_filename("lume_services.docker.files", "environment.yml")
